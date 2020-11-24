c7b5831e3e33d023d316217543b5b2883c344a80
Oleksandr Zdebskyi <technomaster@ukr.net>
2020-11-24 20:46:30 +0200

    done21

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
-  const [click, setClick] = useState(false);
+  const [isOpen, setClick] = useState(false);
 
-  const handleClick = () => setClick(!click);
+  const handleClick = () => setClick(!isOpen);
 
   const onCloseMobMenu = () => setClick(false);
 
@@ -17,9 +17,9 @@ const Navbar = () => {
             LUXURY TRAVEL <i className="fas fa-crown"></i>
           </Link>
           <div className="menu-icon" onClick={handleClick}>
-            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+            <i className={isOpen ? "fas fa-times" : "fas fa-bars"} />
           </div>
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <ul className={isOpen ? "nav-menu active" : "nav-menu"}>
             <li className="nav-item">
               <Link to="/" className="nav-links" onClick={onCloseMobMenu}>
                 Home
